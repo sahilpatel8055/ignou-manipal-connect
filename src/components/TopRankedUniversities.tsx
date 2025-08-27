580f5bf2bdc145f7eefd1455d873671233ffa19e
sahilpatel8055 <Sahilpatelbs@gmail.com>
2025-08-27 15:46:08 +0530

    Update TopRankedUniversities.tsx

--- src/components/TopRankedUniversities.tsx
+++ src/components/TopRankedUniversities.tsx
@@ -51,10 +51,10 @@ const TopRankedUniversities = () => {
             <div className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col h-full">
                 <div className="relative overflow-hidden flex-grow">
                     <img src={universities[0].image} alt={universities[0].name} className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-                    <div className="absolute top-4 left-4">
-                        <span className="relative inline-block py-2 px-6 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
-                            <span className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-red-700 transform rotate-45"></span>
+                    <div className="absolute top-0 left-0 z-10"> {/* Adjusted positioning for the badge container */}
+                        <span className="relative inline-block py-2 px-4 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
                             {universities[0].ranking}
+                            <span className="absolute top-full left-1/2 -translate-x-1/2 -mt-1 w-3 h-3 bg-red-700 transform rotate-45"></span> {/* Adjusted tail positioning */}
                         </span>
                     </div>
                 </div>
@@ -74,10 +74,10 @@ const TopRankedUniversities = () => {
               <div className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col h-full">
                 <div className="relative overflow-hidden">
                     <img src={universities[1].image} alt={universities[1].name} className="w-full object-cover transition-transform duration-300 group-hover:scale-105" />
-                    <div className="absolute top-4 left-4">
-                        <span className="relative inline-block py-2 px-6 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
-                            <span className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-red-700 transform rotate-45"></span>
+                    <div className="absolute top-0 left-0 z-10"> {/* Adjusted positioning for the badge container */}
+                        <span className="relative inline-block py-2 px-4 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
                             {universities[1].ranking}
+                            <span className="absolute top-full left-1/2 -translate-x-1/2 -mt-1 w-3 h-3 bg-red-700 transform rotate-45"></span> {/* Adjusted tail positioning */}
                         </span>
                     </div>
                 </div>
@@ -97,10 +97,10 @@ const TopRankedUniversities = () => {
               <div className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col h-full">
                 <div className="relative overflow-hidden">
                     <img src={universities[2].image} alt={universities[2].name} className="w-full object-cover transition-transform duration-300 group-hover:scale-105" />
-                    <div className="absolute top-4 left-4">
-                        <span className="relative inline-block py-2 px-6 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
-                            <span className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-red-700 transform rotate-45"></span>
+                    <div className="absolute top-0 left-0 z-10"> {/* Adjusted positioning for the badge container */}
+                        <span className="relative inline-block py-2 px-4 text-xs font-bold text-white uppercase bg-red-600 rounded-b-lg">
                             {universities[2].ranking}
+                            <span className="absolute top-full left-1/2 -translate-x-1/2 -mt-1 w-3 h-3 bg-red-700 transform rotate-45"></span> {/* Adjusted tail positioning */}
                         </span>
                     </div>
                 </div>
